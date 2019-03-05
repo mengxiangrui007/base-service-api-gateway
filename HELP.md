d8b68ba4b510cddaaf93ea7d3b72fbf694e3dd42
mengxiangrui003 <mengxiangrui003@lianjia.com>
2019-03-05 15:33:25 +0800

    first commit

---
+++ HELP.md
@@ -0,0 +1,7 @@
+# Getting Started
+
+### Guides
+The following guides illustrates how to use certain features concretely:
+
+* [Using Spring Cloud Gateway](https://github.com/spring-cloud-samples/spring-cloud-gateway-sample)
+
---
+++ src/test/java/com/risen/base/baseserviceapigateway/BaseServiceApiGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.risen.base.baseserviceapigateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BaseServiceApiGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
