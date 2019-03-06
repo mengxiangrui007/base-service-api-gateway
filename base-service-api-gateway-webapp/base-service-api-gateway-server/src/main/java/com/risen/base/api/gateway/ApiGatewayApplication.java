9df250eefb6a7b3d9699ea6e024c5214f8953f2a
mengxiangrui003 <mengxiangrui003@lianjia.com>
2019-03-06 18:41:35 +0800

    del biz

--- base-service-api-gateway-webapp/base-service-api-gateway-server/src/main/java/com/risen/base/api/gateway/ApiGatewayApplication.java
+++ base-service-api-gateway-webapp/base-service-api-gateway-server/src/main/java/com/risen/base/api/gateway/ApiGatewayApplication.java
@@ -3,21 +3,16 @@ package com.risen.base.api.gateway;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(scanBasePackages = {"com.risen"})
 @MapperScan(value = "com.risen.base.api.gateway.mapper")
+@EnableDiscoveryClient
 @RestController
 public class ApiGatewayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
-
-    @RequestMapping("/hystrixfallback")
-    public String hystrixfallback() {
-        return "This is a fallback";
-    }
 }
