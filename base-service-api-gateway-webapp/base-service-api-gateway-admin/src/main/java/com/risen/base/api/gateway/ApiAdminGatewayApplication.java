6cce4a4dde04f5298de4543500bc652b87f9bb48
mengxiangrui003 <mengxiangrui003@lianjia.com>
2019-03-08 14:33:51 +0800

    update

--- base-service-api-gateway-webapp/base-service-api-gateway-admin/src/main/java/com/risen/base/api/gateway/ApiAdminGatewayApplication.java
+++ base-service-api-gateway-webapp/base-service-api-gateway-admin/src/main/java/com/risen/base/api/gateway/ApiAdminGatewayApplication.java
@@ -3,14 +3,13 @@ package com.risen.base.api.gateway;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(scanBasePackages = {"com.risen"})
 @MapperScan(value = "com.risen.base.api.gateway.mapper")
-@EnableDiscoveryClient
-public class ApiGatewayApplication {
+public class ApiAdminGatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args);
+        SpringApplication.run(ApiAdminGatewayApplication.class, args);
     }
 
 }
