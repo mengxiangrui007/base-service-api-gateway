15efad51a0029b93ab6f2275c397abe55bd73e45
mengxiangrui003 <mengxiangrui003@lianjia.com>
2019-06-04 13:49:42 +0800

    update

--- base-service-api-gateway-limit/base-service-api-gateway-limit-sentinel/src/main/java/com/risen/base/api/gateway/sentinel/conf/ApiGatewaySentinelAutoConfiguration.java
+++ base-service-api-gateway-limit/base-service-api-gateway-limit-sentinel/src/main/java/com/risen/base/api/gateway/sentinel/conf/ApiGatewaySentinelAutoConfiguration.java
@@ -1,15 +1,24 @@
 package com.risen.base.api.gateway.sentinel.conf;
 
+import com.alibaba.csp.sentinel.adapter.gateway.sc.SentinelGatewayFilter;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.exception.SentinelGatewayBlockExceptionHandler;
 import com.risen.base.api.gateway.config.ApiGatewayAutoConfiguration;
-import com.risen.base.api.gateway.sentinel.filter.SentinelFilter;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.web.reactive.DispatcherHandler;
+import org.springframework.web.reactive.result.view.ViewResolver;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Sentinel  config
@@ -23,9 +32,20 @@ import org.springframework.web.reactive.DispatcherHandler;
 @EnableConfigurationProperties({ApiGatewaySentinelProperties.class})
 @ConditionalOnBean(ApiGatewayAutoConfiguration.class)
 public class ApiGatewaySentinelAutoConfiguration {
+
     @Bean
-    @ConditionalOnMissingBean
-    public SentinelFilter sentinelFilter() {
-        return new SentinelFilter();
+    @Order(-1)
+    public GlobalFilter sentinelGatewayFilter() {
+        return new SentinelGatewayFilter();
     }
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SentinelGatewayBlockExceptionHandler sentinelGatewayBlockExceptionHandler(ObjectProvider<List<ViewResolver>> viewResolversProvider,
+                                                                                     ServerCodecConfigurer serverCodecConfigurer) {
+        // Register the block exception handler for Spring Cloud Gateway.
+        List<ViewResolver> viewResolvers = viewResolversProvider.getIfAvailable(Collections::emptyList);
+        return new SentinelGatewayBlockExceptionHandler(viewResolvers, serverCodecConfigurer);
+    }
+
 }
